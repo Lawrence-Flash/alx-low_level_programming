a3a6db7a0b92c22c0ad5c38e05bb7a64107cb0a6
Lawrence-Flash <lawrencetshabalala253@gmail.com>
2023-02-19 11:11:41 -0800

    updates

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,10 @@
 /**
  * Description: main - Prints the sizes of each data types
  * Return: always 0 
+ *
  */
-int main(void)
-{
+ int main(void)
+ {
 	int a;
 	long int b;
 	long long int c;
@@ -13,8 +14,9 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s) \n", (unsigned long) sizeof(d));
 	printf("Size of an int: %lu byte(s) \n", (unsigned long) sizeof(a));
-	printf("Size of a long int: %lu byte(s) \n", (unsigned long) sizeof(b));
+	printf("Size of a long int: %lu byte(s) \n", (unsigned long) sizeof(a));
 	printf("Size of a long long int: %lu byte(s) \n", (unsigned long) sizeof(c));
-	printf("Size of a float: %lu byte(s) \n",(unsigned long) sizeof(f));
+	printf("Size of a float: %lu byte(s) \n", (unsigned long) sizeof(f));
+
 	return (0);	
-}	
+ } 	

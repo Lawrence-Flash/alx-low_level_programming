b85d5330a05de0b90e07e140c785ad62d2bbf472
Lawrence-Flash <lawrencetshabalala253@gmail.com>
2023-02-20 19:49:12 -0800

    updates

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * Description: main - Prints out the last part of a quote in the standard error
+ * Description: main- Prints out the last part of a quote in the standard error
  * Return: 1 if successful
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }

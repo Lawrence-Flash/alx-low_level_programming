12e1d0fe9d7176c6f8dcf365bf04b423ef721f7c
Lawrence-Flash <lawrencetshabalala253@gmail.com>
2023-03-02 02:08:23 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenants two strings
+ * @dest: String
+ * @src: String
+ * Return: String
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, l;
+
+	for (l =0; dest[l] != '\0'; l++)
+	{
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[l + i] = src[i];
+	}
+	return (dest);
+}

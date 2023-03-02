037d20e579fd1dee8affeec89ae9e14aab9ac4ae
Lawrence-Flash <lawrencetshabalala253@gmail.com>
2023-03-02 06:47:19 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,23 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
 	{
-	s1++;
-	s2++;
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		else if (*s1 == '\0' || *s2 == '\0')
+		{
+			break;
+		}
 	}
-	return (*s1 - *s2);
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
